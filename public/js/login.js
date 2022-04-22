ba970d0e8f0449a3bd6c59a13c51fdaf3c827d2c
Mikayla Kzinowek <mkzinowek@eagles.bridgewater.edu>
2022-04-22 11:11:55 -0400

    commit

--- public/js/login.js
+++ public/js/login.js
@@ -13,9 +13,9 @@ loginForm.addEventListener("submit", async(e) => {
     const data = { email, password }
 
   //  const url = 'http://localhost:2008/users/login'
-  const url = "https://https://mikayla-kzin-web-page/login"
+ // const url = "https://https://mikayla-kzin-web-page/login"
 
-  //const url = 'https://mikayla-app-api/users/login'
+  const url = 'https://mikayla-app-api/users/login'
 
     const options = {
         method: "POST",
